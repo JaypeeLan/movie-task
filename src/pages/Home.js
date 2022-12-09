02dc645fd0a6748c130412e79c1797fd25995f8f
JaypeeLan <jplaniran01@gmail.com>
2022-12-09 15:27:22 +0100

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+// import backgroundImage from "../files/hero_bg.png"
+
+const Home = () => {
+  return (
+    <div>
+      {/* ----------------------- */}
+      <nav>
+        <div>
+          <p>MyTestApp</p>
+        </div>
+      </nav>
+      {/* ----------------------------- */}
+      <section id="hero">
+        <div className="heroText">
+          <p> Watch something incredible.</p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
